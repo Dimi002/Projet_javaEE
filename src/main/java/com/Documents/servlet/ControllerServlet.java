968a0895d4382fa510ea1a6d2b76a08dbd5b5116
Donald Feuzing <ntemmado@gmail.com>
2022-06-05 04:50:39 +0200

    tache donald

--- src/main/java/com/Documents/servlet/ControllerServlet.java
+++ src/main/java/com/Documents/servlet/ControllerServlet.java
@@ -1,6 +1,8 @@
 package com.Documents.servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.Document.bdd.Authentification;
 import com.Document.bdd.Utilisateur;
+import com.Document.bdd.Cours;
 
 /**
  * Servlet implementation class ControllerServlet
@@ -36,6 +39,11 @@ public class ControllerServlet extends HttpServlet {
 		}else if(action.equals("inscription")){
 			this.getServletContext().getRequestDispatcher("/WEB-INF/inscription.jsp").forward(request, response);
 		}else if(action.equals("cours")){
+			Cours cour = new Cours();
+			String classe = request.getParameter("classe");
+			List<String> matieres = cour.GetListCours();
+			request.setAttribute("matieres",matieres );
+			request.setAttribute("classe",classe );
 			this.getServletContext().getRequestDispatcher("/WEB-INF/cours.jsp").forward(request, response);
 		}else if(action.equals("epreuves")){
 			this.getServletContext().getRequestDispatcher("/WEB-INF/epreuves.jsp").forward(request, response);
@@ -49,15 +57,29 @@ public class ControllerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
-		String userName = request.getParameter("username");
-		String passWord = request.getParameter("password");
 		
-		Authentification authentification = new Authentification();
-		String _nom =authentification.EffectuConnexion(userName,passWord).getNom();
-		String _prenom =authentification.EffectuConnexion(userName,passWord).getPrenom();
-	    request.setAttribute("nom",_nom );
-		request.setAttribute("prenom",_prenom );
-		this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+		String action=request.getParameter("action");
+		if(action==null || action.equals("index")) { 
+			String userName = request.getParameter("username");
+			String passWord = request.getParameter("password");
+			
+			Authentification authentification = new Authentification();
+			String _nom =authentification.EffectuConnexion(userName,passWord).getNom();
+			String _prenom =authentification.EffectuConnexion(userName,passWord).getPrenom();
+		    request.setAttribute("nom",_nom );
+			request.setAttribute("prenom",_prenom );
+			this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+		}else if(action.equals("inscription")){
+			this.getServletContext().getRequestDispatcher("/WEB-INF/inscription.jsp").forward(request, response);
+		}else if(action.equals("cours")){
+			
+			this.getServletContext().getRequestDispatcher("/WEB-INF/cours.jsp").forward(request, response);
+		}else if(action.equals("epreuves")){
+			this.getServletContext().getRequestDispatcher("/WEB-INF/epreuves.jsp").forward(request, response);
+		}else {
+			this.getServletContext().getRequestDispatcher("/WEB-INF/404.jsp").forward(request, response);
+		}
+		
 		
 		
 	}
--- src/main/webapp/WEB-INF/cours.jsp
+++ src/main/webapp/WEB-INF/cours.jsp
@@ -9,6 +9,7 @@
 <style>
 	<%@ include file="assets/css/index.css" %>
 	<%@ include file="assets/css/nav.css" %>
+	<%@ include file="assets/css/courEpreuve.css" %>
 </style>
 </head>
 <body>
@@ -17,7 +18,43 @@
 	<c:import url="inc/nav.jsp"/>
 	
 	
-	code cours ici
+	<section class="section_cours">
+		<section class="section_gauche_cours">
+			<article class="section_nom_classe">
+			<c:out value="${ classe }" />
+			</article>
+			
+			<article class="section_nom_matieres">
+			
+			 <c:forEach var="item" items="${ matieres }">
+                <a><div class="section_titre_matiere">   <c:out value="${ item }" /> </div></a>
+             </c:forEach>
+				
+			</article>
+		
+		</section>
+		
+		
+		<section class="section_droite_cours">
+			<article class="section_recherche">
+				<input type="search" placeholder="rechercher" class="section_input_recherche" /><div><a href="">rechercher</a></div>
+			</article>
+			
+			<article class="section_liste_cours">
+				<table>
+					<tr>
+						<td width="10%"> image </td>
+						<td width="38%"> nom </td>
+						<td width="25%"> date </td>
+						<td width="12%"> <a href="" class="telechager"><div>voir</div></a> </td>
+						<td width="12%"> <a href="" class="telechager"><div>telecharger</div></a> </td>
+					</tr>
+				</table>
+			</article>
+		</section>
+	
+	
+	</section>
 	
 	
 	<c:import url="inc/footer.jsp"/>
